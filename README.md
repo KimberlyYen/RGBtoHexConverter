c7b84bb64cde446e47ca2276e76054c8d6120b4f
kimberly yen <kim223305@example.com>
2023-03-03 10:41:01 +0800

    [Padding] CSS property erro.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 https://lighthouse.alphacamp.co/courses/40/assignments/1236
 
 [IT 幫幫忙](https://ithelp.ithome.com.tw/articles/10279641)
+
+[CSSStyleDeclaration.setProperty()](https://developer.mozilla.org/en-US/docs/Web/API/CSSStyleDeclaration/setProperty)
--- index.js
+++ index.js
@@ -1,53 +1,42 @@
 // GOAL ➡️ 當我點 Convert 的時候進行，十六進制轉換
+
 //  十六進制轉換 1. 拿到三個數字
 //  十六進制轉換 2. 套用公式得到十六進制
 //  十六進制轉換 3. 給到 大 BOX 顯示
+
+
 let button = document.querySelector("button");
-let color = document.querySelector(".text");
 
-const colorR = document.querySelector("#r-color").value;
-const colorG = document.querySelector("#g-color").value;
-const colorB = document.querySelector("#b-color").value;
-const hex = document.querySelector("#hex-color").value;
+// 當我點 Convert 的時候
+button.addEventListener("click", () => {
+  const colorR = document.querySelector("#r-color").value;
+  const colorG = document.querySelector("#g-color").value;
+  const colorB = document.querySelector("#b-color").value;
 
-// 當我點 convert 的時候
-button.addEventListener("click", (event) => {
-  return rgbToHex(colorR, colorG, colorB);
+  //  十六進制轉換 1. 拿到三個數字
+  // console.log(colorR, colorG, colorB)
+  rgbToHex(colorR, colorG, colorB);
+  hex = 333333
+  
 });
+
 
 function rgbToHex(R, G, B) {
   const hexDigit = [R, G, B]
-    .map((item) => {
-      return Number(item).toString(16).padStart(2, "0");
-    })
-    .join("");
-
-  return console.log(hexDigit);
+  .map((item) => {
+    return Number(item).toString(16).padStart(2, "0");
+  })
+  .join("");
+  
+  //  十六進制轉換 2. 套用公式得到十六進制
+  console.log("#" + `${hexDigit}`)
 }
 
-// let getRGB = color.addEventListener("click", () => {
-//   return console.log("colorR, colorG, colorB");
-// });
-// // 印出來 RGB 的 input 陣列
-// let array = [];
-// function returnColor(value) {
-//   if (array.length < 4) {
-//     array.push(Number(value));
-//   } else {
-//     array.shift();
-//     array.pop();
-//   }
+// //  十六進制轉換 3. 給到 HEX CSS 變數 顯示於畫面
+// const newBgColor = rgbToHex();
+// console.log(newBgColor)
+// // const box = document.querySelector(".hex");
+// // box.style.setProperty("background-color", newBgColor);
 
-//   if ((array.length = 3)) {
-//     console.log(array);
-//     convert(array);
-//   }
-// }
+// // return "#" + `${hexDigit}`
 
-// // 套用 RGB 轉換公式
-
-// // 運用變數
-
-// // 給予到右邊的顏色盒子
-
-// // 結束
